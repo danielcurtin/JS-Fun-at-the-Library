bb5a7f55c80e57eeb1ab7479c1830359bf4c41f2
Daniel Curtin <dcurtin.work@gmail.com>
2022-11-30 17:31:27 -0700

    Pass first 8 tests

--- src/book.js
+++ src/book.js
@@ -1,11 +1,25 @@
-function createTitle() {
-  
-}
+function createTitle(title) {
+  return title = `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  };
+  return mainCharacter;
+};
+
+function saveReview(review, array) {
+  var reviews = array;
+  reviews.push(review);
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
