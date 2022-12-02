d5dc86c067cf3ecf44f7fccec48624e9b1d6f7bf
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-01 22:36:01 -0700

    Pass all library tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,58 @@
+function createLibrary (name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      nonFiction: [],
+      fiction: []
+    },
+  };
+  return library;
+};
 
+function addBook (library, book) {
+  if (book.genre === "fantasy") {
+    library.shelves.fantasy.push(book);
+  } else if (book.genre === "nonFiction") {
+    library.shelves.nonFiction.push(book);
+  } else {
+    library.shelves.fiction.push(book);
+  };
+};
+
+function checkoutBook (library, book, genre) {
+  if (genre === "fantasy") {
+    for (var i = 0; i < library.shelves.fantasy.length + 1; i++) {
+      if (library.shelves.fantasy[0] !== undefined && book === library.shelves.fantasy[i].title) {
+        library.shelves.fantasy.splice(library.shelves.fantasy.indexOf(book), 1);
+        return `You have now checked out ${book} from the ${library.name}`;
+      } else {
+        return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+      };
+    };
+  } else if (genre === "nonFiction") {
+    for (var i = 0; i < library.shelves.nonFiction.length + 1; i++) {
+      if (library.shelves.nonFiction[0] !== undefined && book === library.shelves.nonFiction[i].title) {
+        library.shelves.nonFiction.splice(library.shelves.nonFiction.indexOf(book), 1);
+        return `You have now checked out ${book} from the ${library.name}`;
+      } else {
+        return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+      };
+    };
+  } else {
+    for (var i = 0; i < library.shelves.fiction.length + 1; i++) {
+      if (library.shelves.fiction[0] !== undefined && book === library.shelves.fiction[i].title) {
+        library.shelves.fiction.splice(library.shelves.fiction.indexOf(book), 1);
+        return `You have now checked out ${book} from the ${library.name}`;
+      } else {
+        return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
+      };
+    };
+  };
+};
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
