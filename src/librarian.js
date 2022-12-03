5fb5b1b658df0f2d3300c7e522eff38a37f02d0f
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-03 13:53:09 -0700

    Pass all librarian tests

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,25 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+  greetPatron(name, isMorning) {
+    if (isMorning === true) {
+      return (`Good morning, ${name}!`);
+    } else {
+      return (`Hello, ${name}!`);
+    }
+  }
+  findBook(inputTitle) {
+    if (this.library.shelves.fantasy[0].title === inputTitle) {
+      this.library.shelves.fantasy.splice(0, 1);
+      return `Yes, we have ${inputTitle}`;
+    } else {
+      return `Sorry, we do not have ${inputTitle}`;
+    }
+  }
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * 0.25);
   }
 }
 
